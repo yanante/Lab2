e6ff28ef9f8286a92661b5869d5d60fbdb0e0ae1
Yan Saidashev <77618038+yanante@users.noreply.github.com>
2025-04-03 14:28:55 +0600

    Update StringProcessorTest.java

--- src/test/java/org.example/StringProcessorTest.java
+++ src/test/java/org.example/StringProcessorTest.java
@@ -6,29 +6,30 @@ import org.junit.jupiter.api.Test;
 
 public class StringProcessorTest {
     @Test
-    public void multiplyOfString() {
-        Assertions.assertEquals("", StringProcessor.multiplyOfString("", 3));
-        Assertions.assertEquals("123123123", StringProcessor.multiplyOfString("123", 3));
-        Assertions.assertEquals("", StringProcessor.multiplyOfString("", 3));
-        Assertions.assertEquals("", StringProcessor.multiplyOfString("", 0));
+    public void multiplyString() {
+        Assertions.assertEquals("абвгабвгабвг", StringProcessor.multiplyString("абвг", 3));
+        Assertions.assertEquals("123123123", StringProcessor.multiplyString("123", 3));
+        Assertions.assertEquals("", StringProcessor.multiplyString("", 3));
+        Assertions.assertEquals("", StringProcessor.multiplyString("", 0));
     }
     @Test
-    public void multiplyOfError() {
-        Assertions.assertThrows(IllegalArgumentException.class, () -> StringProcessor.multiplyOfString("", -1));
+    public void multiplyError() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> StringProcessor.multiplyString("", -1));
     }
 
     @Test
     public void countEntryTest(){
         Assertions.assertEquals(3,StringProcessor.countEntryTest("123123123","123"));
         Assertions.assertEquals(2,StringProcessor.countEntryTest("aaa","aa"));
-        //Assertions.assertEquals(3,StringProcessor.searchForSubstring("123","124"));
+        Assertions.assertEquals(3,StringProcessor.searchForSubstring("123","124"));
 
     }
 
     @Test
     public void replaceNumbers(){
         Assertions.assertEquals("У человека один нос, два уха и четыре больших пальца", StringProcessor.replaceNumbers("У человека 1 нос, 2 уха и 4 больших пальца"));
-        Assertions.assertEquals("одинодинодинодин4", StringProcessor.replaceNumbers("11114"));
+        Assertions.assertEquals("одиндватри", StringProcessor.replaceNumbers("123"));
+        Assertions.assertEquals("одиноодиндва", StringProcessor.replaceNumbers("1о12"));
     }
 
     @Test
